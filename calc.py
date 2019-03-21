564f08e9c633c42f9b4cffb73f5fcce00b814253
Dawid <dawid.debo2.pl@gmail.com>
2019-03-21 19:31:24 +0100

    calc.py1

---
+++ calc.py
@@ -0,0 +1,40 @@
+def calc_add (a, b):
+    print('{} + {} ='.format(str(a), str(b)), end=' ')
+    return a + b
+    
+def calc_substrack(a, b):
+    print('{} - {} ='.format(str(a), str(b)), end=' ')
+    return a - b
+
+def calc_multiple(a, b):
+    print('{} * {} ='.format(str(a), str(b)), end=' ')
+    return a * b
+
+def calc_devide(a, b):
+    print('{} / {} ='.format(str(a), str(b)), end=' ')
+    return a / b
+
+def check_operation_name(operation):
+    if operation in method_dict:
+        return method_dict[operation]
+    operation = input('Niepoprawne dzialanie, podaj jeszcze raz: dodawanie, odejmowanie, mnozenie, dzielenie : ')
+    check_operation_name(operation)
+
+method_dict = {
+    'dodawanie': calc_add,
+    'odejmowanie': calc_substrack,
+    'mnozenie': calc_multiple,
+    'dzielenie': calc_devide
+} 
+     
+
+if __name__ == "__main__":
+    print('Witam w moim kalkulatorze :)')
+    while True:
+        operation = input('Podaj dzialanie: dodawanie, odejmowanie, mnozenie, dzielenie : ')
+        if operation == 'exit':
+            break
+        operation_def = check_operation_name(operation)
+        a = input('Podaj pierwsza liczbe: ')
+        b = input('podaj druga liczbe: ')
+        print( operation_def(int(a), int(b)) )
